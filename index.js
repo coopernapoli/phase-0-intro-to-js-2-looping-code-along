6f65ec2e747ea14b2406d54bbb61c0fb11fb95d2
Sean <napolisc10@gmail.com>
2022-12-20 12:39:00 -0700

    commit tree ac4cd18b6f97fcd7b72e1339551a31551f1f4c0b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+let names = [];
+let occasion = "birthday";
+
+function writeCards(names, occasion) {
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        
+        const message = `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`;
+
+        messages.push(message);
+    }
+
+return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number --;
+    }
+}
